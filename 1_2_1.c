e4ec291f36a06637d5426f43e367a5bff2a5eaf3
dle-oxio <35052949+dle-oxio@users.noreply.github.com>
2018-12-02 14:45:17 +0800

    add

---
+++ 1_2_1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int i,j;
+    int x=5;
+    int y=10;
+    char input;
+
+    while(1)
+    {
+        system("cls");
+        for(i=0;i<x;i++)
+        {
+            printf("\n");
+        }
+        for(j=0;j<y;j++)
+        {
+            printf(" ");
+        }
+        printf("*");
+        printf("\n");
+
+        scanf("%c",&input);
+        if(input == 'a')
+            y--;
+        if(input == 'd')
+            y++;
+        if(input == 'w')
+            x--;
+        if(input == 's')
+            x++;
+    }
+    return 0;
+}
